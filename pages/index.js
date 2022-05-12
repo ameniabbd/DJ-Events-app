bf26100256bad617221ebb3fff3dd104612fe3ae
Ameni <ameni.abdelghani@steps.tn>
2022-05-12 11:58:05 +0100

    404 page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "./components/Layout";
+import Layout from "../components/Layout";
 import Image from "next/image";
 import styles from "../styles/Layout.module.css";
 
