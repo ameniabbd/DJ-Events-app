64d73a63e515db53c8fae227ae1a55ad7488b957
Ameni <ameni.abdelghani@steps.tn>
2022-05-12 09:55:18 +0100

    fix git

---
+++ pages/events/[slug].js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function EventPage() {
+    return(
+  <div>
+    <h1> Events </h1>
+  </div>
+    )}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+export default function EventsPage() {
+    return(
+  <div>
+    <h1>My Events </h1>
+  </div>
+    )}
