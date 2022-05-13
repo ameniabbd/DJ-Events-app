8b30797526e9a442686dd6b3d47d80477f7e113e
Ameni <ameni.abdelghani@steps.tn>
2022-05-13 09:21:01 +0100

    display events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,33 @@
-import React from "react";
+import Head from "next/head";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/Layout.module.css";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My Events </h1>
+      <h1> Events</h1>
+      {events.length === 0 && <h3>NO EVENTS TO SHOW </h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt}></EventItem>
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events:events.slice(0,3) },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 import Head from "next/head";
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
 import Image from "next/image";
 import styles from "@/styles/Layout.module.css";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({events}) {
   return <Layout>
-<h1>Home</h1>
-  </Layout>;
+<h1>Upcoming Events</h1>
+{events.length === 0 && <h3>NO EVENTS TO SHOW </h3>}
+{events.map(evt=>(
+  <EventItem key={evt.id} evt={evt}>
+  </EventItem>
+))}
+  </Layout>  
 }
+ export async function getStaticProps (){
+  const res= await fetch(`${API_URL}/api/events`)
+  const events=await res.json()
+
+  return{
+    props:{events},
+    revalidate:1,  
+ }
+ }
